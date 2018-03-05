57b5229b883a40d99c6ab280ea08c2680fba09dd
nshumtc <mastershumakov@gmail.com>
2018-03-05 20:08:32 +0200

    All_gref

--- first_task.py
+++ first_task.py
@@ -20,15 +20,38 @@ class TestAddingToCommentsList(unittest.TestCase):
         make_new_comment.send_keys(self.test_comment, Keys.ENTER)
         add_all_categories = self.driver.find_element_by_name("AllSelect")
         add_all_categories.send_keys(Keys.ENTER)
-        self.driver.find_element_by_xpath('//*[@id="editor-navigation"]/input[2]').click()
+        self.driver.find_element_by_xpath(
+            '//*[@id="editor-navigation"]/input[2]').click()
         self.driver.find_element_by_xpath('//*[@id="main"]/div/div[5]/form/table/thead/tr/th[2]/a').click()
-        self.driver.find_element_by_xpath(''
-                                          '//*[@id="main"]/div/div[5]/'
-                                          'form/table/tbody/tr[1]'
-                                          '/td[1]/input[1]').click()
-        self.driver.find_element_by_xpath('//*[@id="command-navigation"]/input[2]').click()
-        comparison_value_getting = self.driver.find_element_by_id("Text").get_attribute("value")
-        self.assertEqual(comparison_value_getting, self.test_comment)
+        list_el = self.driver.find_element_by_class_name("webgrid-footer")
+        list_a = list_el.find_elements_by_tag_name("a")
+        href = list()
+        for link_a in list_a:
+            href.append(link_a.get_attribute("href"))
+
+        for ref in href:
+            self.driver.get(ref)
+
+        # arr_webgrid_row_style = self.driver.find_elements_by_class_name("webgrid-row-style")
+        # arr_webgrid_alt_style = self.driver.find_elements_by_class_name("webgrid-alternating-row")
+        # for arr in arr_webgrid_row_style:
+        #     if arr.text in self.test_comment:
+        #         print("Element is founded in arr_webgrid_row_style")
+        #     else:
+        #         print("Element in not founded in arr_webgrid_row_style")
+        # for arr in arr_webgrid_alt_style:
+        #     if arr.text in self.test_comment:
+        #         print("Element is founded")
+        #     else:
+        #         print("Element in not founded")
+        # self.driver.find_element_by_xpath('//*[@id="main"]/div/div[5]/form/table/thead/tr/th[2]/a').click()
+        # self.driver.find_element_by_xpath(''
+        #                                   '//*[@id="main"]/div/div[5]/'
+        #                                   'form/table/tbody/tr[1]'
+        #                                   '/td[1]/input[1]').click()
+        # self.driver.find_element_by_xpath('//*[@id="command-navigation"]/input[2]').click()
+        # comparison_value_getting = self.driver.find_element_by_id("Text").get_attribute("value")
+        # self.assertEqual(comparison_value_getting, self.test_comment)
         time.sleep(5)
 
     def tearDown(self):
