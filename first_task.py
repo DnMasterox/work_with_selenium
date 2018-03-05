f81e199b9d583373c5d526999b1d5349c7e786fc
nshumtc <mastershumakov@gmail.com>
2018-03-05 19:04:00 +0200

    Chrome add

--- first_task.py
+++ first_task.py
@@ -10,7 +10,7 @@ class TestAddingToCommentsList(unittest.TestCase):
     def setUp(self):
         self.test_comment = "First test comment"
         self.driver = webdriver.Chrome(
-            executable_path="C:\Program Files (x86)\Python36-32\Scripts\chromedriver.exe")
+            executable_path="chromedriver.exe")
         self.driver.get("http://commentssprintone.azurewebsites.net")
 
     def test_adding_to_comments_list(self):
@@ -22,7 +22,10 @@ class TestAddingToCommentsList(unittest.TestCase):
         add_all_categories.send_keys(Keys.ENTER)
         self.driver.find_element_by_xpath('//*[@id="editor-navigation"]/input[2]').click()
         self.driver.find_element_by_xpath('//*[@id="main"]/div/div[5]/form/table/thead/tr/th[2]/a').click()
-        self.driver.find_element_by_xpath('//*[@id="main"]/div/div[5]/form/table/tbody/tr[1]/td[1]/input[1]').click()
+        self.driver.find_element_by_xpath(''
+                                          '//*[@id="main"]/div/div[5]/'
+                                          'form/table/tbody/tr[1]'
+                                          '/td[1]/input[1]').click()
         self.driver.find_element_by_xpath('//*[@id="command-navigation"]/input[2]').click()
         comparison_value_getting = self.driver.find_element_by_id("Text").get_attribute("value")
         self.assertEqual(comparison_value_getting, self.test_comment)
