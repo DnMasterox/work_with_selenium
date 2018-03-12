d24b7496f7253e601dacdb54aee71bd9b1e8f9b1
nshumakov <mastershumakov@gmail.com>
2018-03-12 19:04:11 +0200

    Constants added

--- app_logics/preparing_for_buttons_tests.py
+++ app_logics/preparing_for_buttons_tests.py
@@ -2,6 +2,10 @@ from selenium.webdriver.common.keys import Keys
 
 __author__ = 'nshumakov'
 
+DIGIT_ONE = 1
+DIGIT_TWO = 2
+DIGIT_FIVE = 5
+
 
 def prepare_new_button_for_first_test(driver, text_comment,
                                       number_comment) -> None:
@@ -14,7 +18,7 @@ def prepare_new_button_for_first_test(driver, text_comment,
     add_all_categories = driver.find_element_by_name("AllSelect")
     add_all_categories.send_keys(Keys.ENTER)
     save_button = driver.find_element_by_xpath(
-        '//*[@id="editor-navigation"]/input[%d]' % 1)
+        '//*[@id="editor-navigation"]/input[%d]' % DIGIT_ONE)
     save_button.click()
     return_button = driver.find_element_by_xpath(
         '//*[@id="logindisplay"]/a')
@@ -29,7 +33,7 @@ def prepare_new_button_for_second_test(driver, text_comment) -> None:
     add_all_categories = driver.find_element_by_name("AllSelect")
     add_all_categories.send_keys(Keys.ENTER)
     save_and_return_button = driver.find_element_by_xpath(
-        '//*[@id="editor-navigation"]/input[%d]' % 2)
+        '//*[@id="editor-navigation"]/input[%d]' % DIGIT_TWO)
     save_and_return_button.click()
 
 
@@ -55,7 +59,7 @@ def prepare_edit_button_for_test(driver, text_comment) -> None:
         "SelectedId")
     select_comment_for_duplicate.click()
     driver.find_element_by_xpath(
-        "//*[@id='command-navigation']/input[%d]" % 2).click()
+        "//*[@id='command-navigation']/input[%d]" % DIGIT_TWO).click()
     text_field = driver.find_element_by_id("Text")
     text_field.clear()
     text_field.send_keys(text_comment)
@@ -64,13 +68,13 @@ def prepare_edit_button_for_test(driver, text_comment) -> None:
     save_and_return_button.click()
     refresh_comments_list = driver.find_element_by_xpath(
         "//*[@id='main']/div/div[%d]/form/table/thead/tr/th[%d]/a"
-        % (5, 2))
+        % (DIGIT_FIVE, DIGIT_TWO))
     refresh_comments_list.click()
 
 
 def prepare_delete_button_for_tests(driver) -> None:
     refresh_comments_list = driver.find_element_by_xpath(
-        "//*[@id='main']/div/div[%d]/form/table/thead/tr/th[%d]/a" % (5, 2))
+        "//*[@id='main']/div/div[%d]/form/table/thead/tr/th[%d]/a" % (DIGIT_FIVE, DIGIT_TWO))
     refresh_comments_list.click()
     select_comment_for_deleting = driver.find_element_by_name(
         "SelectedId")
